dc2ce134e16c1264b3ef13879cd5652566aa9c88
Weslley Batista <weslleyvitorbatista@gmail.com>
2025-06-15 05:04:23 -0300

    correção animacao safari ios

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   requestAnimationFrame(() => {
-    animateCarousel();
+    requestAnimationFrame(() => {
+      animateCarousel();
+    });
   });
   enableNavbar();
   enableAbout();
